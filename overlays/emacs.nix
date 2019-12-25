013259b8b86edaa0e4494fb236677c0fbc07e669
Tom Gustafsson <enigustom@eniram.fi>
2019-12-25 22:26:10 +0200

    Simplify emacs overlay

--- overlays/emacs.nix
+++ overlays/emacs.nix
@@ -1,37 +1,37 @@
-self: super: with self; rec {
-  myEmacsConfig = writeText "default.el" (builtins.readFile ./init.el);
-  myEmacs = emacsWithPackages (epkgs: (with epkgs; [
-    (runCommand "default.el" {} ''
+self: super: {
+  emacs_config = self.writeText "default.el" (builtins.readFile ./init.el);
+  emacs = self.emacsWithPackages (epkgs: (with epkgs.melpaPackages; [
+    use-package
+    hydra
+    ivy
+    counsel
+    swiper
+    transient
+    magit
+    which-key
+    exec-path-from-shell
+    expand-region
+    multiple-cursors
+    json-mode
+    leuven-theme
+    highlight-indentation
+    yaml-mode
+    wgrep
+    phi-search
+    docker
+    restclient
+    ob-restclient
+    htmlize
+    diredfl
+    python-pytest
+    dired-k
+    nix-mode
+    anaconda-mode
+    ivy-pass
+  ]) ++ (with epkgs.elpaPackages; [
+    csv-mode
+  ]) ++ [ (self.runCommand "default.el" {} ''
 mkdir -p $out/share/emacs/site-lisp
-cp ${myEmacsConfig} $out/share/emacs/site-lisp/default.el
-'')
-    melpaPackages.use-package
-    melpaPackages.hydra
-    melpaPackages.ivy
-    melpaPackages.counsel
-    melpaPackages.swiper
-    melpaPackages.transient
-    melpaPackages.magit
-    melpaPackages.which-key
-    melpaPackages.exec-path-from-shell
-    melpaPackages.expand-region
-    melpaPackages.multiple-cursors
-    melpaPackages.json-mode
-    melpaPackages.leuven-theme
-    melpaPackages.highlight-indentation
-    melpaPackages.yaml-mode
-    melpaPackages.wgrep
-    melpaPackages.phi-search
-    melpaPackages.docker
-    melpaPackages.restclient
-    melpaPackages.ob-restclient
-    melpaPackages.htmlize
-    melpaPackages.diredfl
-    melpaPackages.python-pytest
-    melpaPackages.dired-k
-    melpaPackages.nix-mode
-    melpaPackages.anaconda-mode
-    melpaPackages.ivy-pass
-    elpaPackages.csv-mode
-  ]));
+cp ${self.emacs_config} $out/share/emacs/site-lisp/default.el
+'') ]);
 }
